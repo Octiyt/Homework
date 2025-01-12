66bcd043cf6dea705320751928e0d1c74b328fc0
deny <denn.chesnokov@gmail.com>
2025-01-12 12:32:47 +0200

    HOMEWORK

--- HW.py
+++ HW.py
@@ -1,21 +1,27 @@
 result = []
 
-
 def divider(a, b):
     if a < b:
-        raise ValueError("a меньшеb")
+        raise ValueError
     if b > 100:
-        raise IndexError("b більше 100")
+        raise IndexError
     return a / b
 
-
-data = {10: 1, 2: 5, "123": 4, 18: 1, 8: 4}
-
+data = {10: 2, 2: 5, "123": 4, 18: 0, "[]": 15, 8: 4}  # Ключ "[]" — это строка
 
 for key in data:
     try:
         res = divider(key, data[key])
         result.append(res)
+    except ValueError as ve:
+        print(f"ValueError: первое число менше  другого")
+    except IndexError as ie:
+        print(f"IndexError: второе число більше 100 ")
+    except ZeroDivisionError as zde:
+        print(f"ZeroDivisionError: ділити на ноль")
+    except TypeError as te:
+        print(f"TypeError: {te}")
     except Exception as e:
-        print(f"Ошибка: {e}")
+        print(f"Other Error: {e}")
+
 print(result)
